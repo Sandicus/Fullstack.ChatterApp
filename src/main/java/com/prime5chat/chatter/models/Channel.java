36345a8c56ebb3a32c639536977d8b17266a6792
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-19 16:23:50 -0500

    refactor names to match database schema

---
+++ src/main/java/com/prime5chat/chatter/models/Channel.java
@@ -0,0 +1,25 @@
+package com.prime5chat.chatter.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
+public class Channel {
+
+    @Id
+    private String CHANNEL_NAME;
+
+//    @ManyToMany
+//    private List<User> allowedUsers;
+
+    public String getCHANNEL_NAME() {
+        return CHANNEL_NAME;
+    }
+
+    public void setCHANNEL_NAME(String CHANNEL_NAME) {
+        this.CHANNEL_NAME = CHANNEL_NAME;
+    }
+}
---
+++ src/main/java/com/prime5chat/chatter/models/UsersChannels.java
@@ -0,0 +1,44 @@
+package com.prime5chat.chatter.models;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UsersChannels {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; // had to add as JPA doesn't work without it
+    private  String CHANNEL_NAME;
+    private  String USER_NAME;
+    private Integer USER_TYPE = 1; // default value 1 - user; 2 - moderator; 3 - admin
+
+    public String getCHANNEL_NAME() {
+        return CHANNEL_NAME;
+    }
+
+    public void setCHANNEL_NAME(String CHANNEL_NAME) {
+        this.CHANNEL_NAME = CHANNEL_NAME;
+    }
+
+    public String getUSER_NAME() {
+        return USER_NAME;
+    }
+
+    public void setUSER_NAME(String USER_NAME) {
+        this.USER_NAME = USER_NAME;
+    }
+
+    public Integer getUSER_TYPE() {
+        return USER_TYPE;
+    }
+
+    public void setUSER_TYPE(Integer USER_TYPE) {
+        this.USER_TYPE = USER_TYPE;
+    }
+}
