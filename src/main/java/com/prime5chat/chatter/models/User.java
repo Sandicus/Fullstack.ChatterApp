c9f0f03b7a34967c1828f6e29e2a519806b18212
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-18 19:39:59 -0500

    updated the userid and changed the schema for tables

--- src/main/java/com/prime5chat/chatter/models/User.java
+++ src/main/java/com/prime5chat/chatter/models/User.java
@@ -7,21 +7,11 @@ import java.util.List;
 @Table
 public class User {
     @Id
-    @GeneratedValue
-    private Long id;
     private String UserName;
     private String password;
 //    @ManyToMany
 //    private List<Group> groups;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getUserName() {
         return UserName;
