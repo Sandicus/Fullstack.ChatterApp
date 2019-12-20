f9fdfffa4bf8970333b022be8bdb06cb88fa60a4
k-law21 <kwame@udel.edu>
2019-12-19 21:04:49 -0500

    getters and stters for channel model

--- src/main/java/com/prime5chat/chatter/models/ChatChannel.java
+++ src/main/java/com/prime5chat/chatter/models/ChatChannel.java
@@ -1,18 +1,53 @@
 package com.prime5chat.chatter.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class ChatChannel {
     @Id
-    @GeneratedValue
-    private Long id;
-//
-//    @ManyToMany
-//    private List<User> allowedUsers;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    private Long id;
+
+
+    private String channelName;
+
+
+
+    @ManyToMany
+    private List<ChatUsers> users = new ArrayList<>();
+    private boolean isPrivate;
+
+    public ChatChannel(){}
+    public ChatChannel(String channelName){
+        this.channelName = channelName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setID(Long id){
+            this.id = id;
+    }
+    public String getChannelName(){
+        return channelName;
+    }
+    public void setChannelName(String channelName){
+        this.channelName = channelName;
+    }
+    public boolean isPrivate(){
+        return isPrivate;
+    }
+    public void setPrivate(boolean aPrivate){
+        this.isPrivate = aPrivate;
+    }
+    public List<ChatUsers> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<ChatUsers> users) {
+        this.users = users;
+    }
 }
